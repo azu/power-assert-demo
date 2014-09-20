44710ffbd85a0dbb8efe2f911106c3318bdeaf55
azu <azuciao@gmail.com>
2014-09-20 19:51:53 +0900

    feat(travis): add travis script

---
+++ _travis/deploy-gh-pages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
+echo "This is a pull request. No deployment will be done.";
+  exit 0;
+fi
+if [[ "$TRAVIS_BRANCH" != "master" ]] && [[ "$TRAVIS_BRANCH" != "staging" ]]; then
+echo "This is not a deployable branch.";
+  exit 0;
+fi
+
+
+echo "BUMP HELLO WORLD set up $GH_REPO [via travis] for $GIT_NAME <${GIT_EMAIL}>"
+export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_NAME"
+lastCommit=$(git log --oneline | head -n 1)
+
+echo "=STATUS="
+git status
+
+echo "=COMMIT="
+echo "MESSAGE :" $lastCommit
+
+git add .
+git commit -q -m "Travis build $TRAVIS_BUILD_NUMBER"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
